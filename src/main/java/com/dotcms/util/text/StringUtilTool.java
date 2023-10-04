a064de58c6e9a7cc39ea560fddf9fc97496355c4
Vasyl Petrashchuk <vasyl.petrashchuk@sombrainc.com>
2023-10-04 17:58:09 +0300

    0.3 version release

--- src/main/java/com/dotcms/util/text/StringUtilTool.java
+++ src/main/java/com/dotcms/util/text/StringUtilTool.java
@@ -3,6 +3,8 @@ package com.dotcms.util.text;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.velocity.tools.view.tools.ViewTool;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,5 +19,24 @@ public class StringUtilTool extends StringUtils implements ViewTool {
 
         // The substituted value will be contained in the result variable
         return matcher.replaceAll(substitution);
+    }
+
+    public static Map<String, String> regexToMap(String str, String regex) {
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(str);
+
+
+        LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
+        try {
+            // The substituted value will be contained in the result variable
+            while (matcher.find()) {
+                String key = matcher.group(1);
+                String value = matcher.group(2);
+                linkedHashMap.put(key, value);
+            }
+            return linkedHashMap;
+        } catch (Exception e){
+            return linkedHashMap;
+        }
     }
 }
